87ef0bba370a5ba6286a55e8feaf2054bfd01af7
Ivan Kasatenko <sky.31338@gmail.com>
2012-01-28 23:54:49 +0400

    Some more work

--- test.rb
+++ test.rb
@@ -3,9 +3,23 @@ Bundler.require
 
 require 'ruby-debug'
 
-puts "Hello, world!"
+(1..10).to_a.each do |number|
+  sleep 0.13
+  Thread.new do
+    local_number = number
+    while true
+      # puts "Hello from ##{local_number}"
+      sleep 1
+      local_number += 1
+    end
+  end  
+end
 
-a = 123
-debugger if fork
+puts "Waiting for debugger ..."
+Debugger.wait_connection = true
+Debugger.start_remote
 
+puts "Continuing..."
+
+sleep 30
 puts "Goodbye, world!"
